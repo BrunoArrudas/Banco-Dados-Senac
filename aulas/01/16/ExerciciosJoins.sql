42bde7b5d5235958c74830615f08abe6b6f96f53
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2024-03-08 20:34:40 -0300

    Atualização 0.16

--- aulas/01/16/ExerciciosJoins.sql
+++ aulas/01/16/ExerciciosJoins.sql
@@ -1,5 +1,7 @@
 USE hotel;
 -- ----------------------------------------------------------------------------------------------------
+-- Consultas sem JOINS
+
 -- Consulte todos os clientes de São Paulo(SP)
 SELECT *
 FROM cliente
@@ -13,7 +15,7 @@ WHERE qtdPessoas >= 3 AND desconto = 0;
 -- Selecionar telefones de clientes com ID 1 ou 2, limitando a 2 resultados
 SELECT *
 FROM telefone
-WHERE codCliente = 1 OR 2 LIMIT 2;
+WHERE codCliente = 1 OR codCliente= 2 LIMIT 2;
 
 -- Selecionar chales com capacidades entre 4 e 6, ordenados por valor em alta estação 
 SELECT * 
@@ -28,9 +30,11 @@ WHERE descricaoItem LIKE '%de%' LIMIT 13 OFFSET 2;
 
 -- --------------------------------------------------------------------------------------------------
 -- Consultas com JOINS
+
 -- Selecionar detalhes da hospedagem, incluindo informações do cliente
-SELECT * FROM cliente;
-SELECT * FROM telefone;
+SELECT hospedagem.*, Cliente.nomeCliente
+FROM hospedagem
+Join Cliente ON Hospedagem.codCliente = Cliente.codCliente;
 
 -- Selecionar chalés com os itens associados
 SELECT *
@@ -39,15 +43,22 @@ JOIN chale_item ON chale_item.codchale = chale.codchale
 JOIN item ON chale_item.nomeItem = item.nomeItem;
 
 -- Selecionar serviços utilizados em uma hospedagem
+SELECT *
+FROM hospedagem_servico
+JOIN servico ON hospedagem_servico.codServico = servico.codServico;
+-- WHERE hospedagem_servico.codHospedagem = 1;
 
 -- Consultar os clientes com seus telefones
 SELECT *
 FROM cliente
 JOIN telefone ON cliente.codCliente = telefone.codCliente;
+-- SELECT Cliente.*, GROUP_CONCAT(TELEFONE.TELEFONE)AS TELEFONE
+-- LEFT JOIN Telefone ON Cliente.codCliente = telefone.codCliente
 
-
--- Selecionar chalés ocupados em uma data específica
-
-
+-- Selecionar chalés ocupados em uma data específica 2024-07-18
+SELECT chale.*, hospedagem.dataInicio, hospedagem.dataFim
+FROM chale
+JOIN Hospedagem ON chale.codChale = Hospedagem.codChale
+WHERE '2024-07-18' BETWEEN hospedagem.dataInicio AND hospedagem.dataFim;
 
 
